92bdc9997b5eeccb14525cd6868c91fd62215705
Syed <1221109619@student.mmu.edu.my>
2024-05-05 22:35:00 +0800

    ds

--- tutorialFile/website/models.py
+++ tutorialFile/website/models.py
@@ -2,7 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
+#put test
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
