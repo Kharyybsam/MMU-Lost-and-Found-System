f24ecabc29b0ef4ac0d5ba4251d15a7253656f32
Danish <danishdzul2712@gmail.com>
2024-05-06 21:09:08 +0800

    fetch test

--- tutorialFile/website/models.py
+++ tutorialFile/website/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 #put test
+#fetch test
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
