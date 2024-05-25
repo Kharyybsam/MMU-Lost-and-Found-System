621f1fc2ab1e38f5e1ca12528acea3414ae0b331
Syed <1221109619@student.mmu.edu.my>
2024-05-25 15:31:55 +0800

    Added a placeholder welcome text. Login and signup input boxes do not reset anymore after submit is pressed

--- tutorialFile/website/auth.py
+++ tutorialFile/website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for,session
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
-
+from flask_session import Session
 
 auth = Blueprint('auth', __name__)
 
@@ -17,15 +17,16 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+                flash('Welcome Back ' + current_user.first_name + "!",category='info')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
-
-    return render_template("login.html", user=current_user)
+    email = request.form.get('email', '')
+    password = request.form.get('password','')
+    return render_template("login.html", user=current_user,email=email,password=password)
 
 
 @auth.route('/logout')
@@ -62,5 +63,9 @@ def sign_up():
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-
-    return render_template("sign_up.html", user=current_user)
+    #this 4 is used to remember input
+    first_name = request.form.get('firstName','')
+    password1 = request.form.get('password1','')
+    email = request.form.get('email', '')
+    password2 = request.form.get('password2','')
+    return render_template("sign_up.html", user=current_user,first_name=first_name,password1=password1,email=email,password2=password2)
