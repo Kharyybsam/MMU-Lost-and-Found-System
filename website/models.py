9793e6f3f75d96dcdf4fe443a3b06fd57e4b516e
Danish Dzul <167834778+PlasticLunatic27io@users.noreply.github.com>
2024-06-22 18:00:31 +0800

    Revert "Merge remote-tracking branch 'origin/Danish2'"
    This reverts commit 57fbcac6ed2f8950f54b68ea563f5bfe3c7c277a, reversing
    changes made to db5a5c453f396cfa5bba3b579eea45361299dabb.

--- website/models.py
+++ website/models.py
@@ -1,14 +1,8 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-import pytz
-from datetime import datetime
-from sqlalchemy import event
 
-def remove_microseconds(r):
-    if r is not None:
-        return r.replace(microsecond=0)
-    return r
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,27 +18,16 @@ class Lostitem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000))             
     description = db.Column(db.String(999999))
-    date_lost = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(pytz.timezone('Asia/Kuala_Lumpur')).replace(microsecond=0))
+    date_lost = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     image_file = db.Column(db.Text, nullable=False)
     location = db.Column(db.String(1000), nullable=False)
-
 
 class Founditem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
     description = db.Column(db.String(999999))
-    date_found = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(pytz.timezone('Asia/Kuala_Lumpur')).replace(microsecond=0))
+    date_found = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     image_file = db.Column(db.Text, nullable=False)
     location = db.Column(db.String(1000), nullable=False)
-
-@event.listens_for(Lostitem, 'before_insert')
-@event.listens_for(Lostitem, 'before_update')
-def before_saving_lostitem(mapper, connection, target):
-    target.date_lost = remove_microseconds(target.date_lost)
-
-@event.listens_for(Founditem, 'before_insert')
-@event.listens_for(Founditem, 'before_update')
-def before_saving_founditem(mapper, connection, target):
-    target.date_found = remove_microseconds(target.date_found)
